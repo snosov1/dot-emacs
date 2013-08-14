31682ec251c66710cda701ba9df8499f9a7f9b2c
Sergei Nosov <sergei.nosov@gmail.com>
2013-08-14 12:41:35 +0400

    added script for configuring remote machine for comfortable emacs access via ssh

---
+++ configure-remote.sh
@@ -0,0 +1,5 @@
+# prepend .profile
+cat .profile | ssh $1 'cp .profile .profile.sergei.bak && cat - .profile >.profile.sergei.emacs.dired && cp .profile.sergei.emacs.dired .profile'
+
+# add public key to authorized_keys
+cat ~/.ssh/id_rsa.pub | ssh $1 'cat - >> ~/.ssh/authorized_keys'
