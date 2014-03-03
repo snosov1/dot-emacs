6bd1e7971269a69de827247b00d6f0253b39f195
Sergei Nosov <sergei.nosov@gmail.com>
2014-03-03 16:13:34 +0400

    fix configure-remote.sh to add public key in one go

--- configure-remote.sh
+++ configure-remote.sh
@@ -1,7 +1,5 @@
+# add public key to authorized_keys
+cat ~/.ssh/id_rsa.pub | ssh $1 'mkdir -p .ssh && cat - >>.ssh/authorized_keys'
+
 # prepend .profile
 cat .profile | ssh $1 'cp .profile .profile.sergei.bak && cat - .profile >.profile.sergei.emacs.dired && cp .profile.sergei.emacs.dired .profile'
-
-# add public key to authorized_keys
-ssh $1 mkdir .ssh
-ssh $1 touch .ssh/authorized_keys
-cat ~/.ssh/id_rsa.pub | ssh $1 'cat - >> .ssh/authorized_keys'
