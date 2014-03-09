7ec26dbbbed0fb66b3f49d2b194be79963665a38
Sergei Nosov <sergei.nosov@gmail.com>
2014-03-09 12:56:32 +0400

    move to another repo

--- configure-remote.sh
+++
@@ -1,5 +0,0 @@
-# add public key to authorized_keys
-cat ~/.ssh/id_rsa.pub | ssh $1 'mkdir -p .ssh && cat - >>.ssh/authorized_keys'
-
-# prepend .profile
-cat .profile | ssh $1 'cp .profile .profile.sergei.bak && cat - .profile >.profile.sergei.emacs.dired && cp .profile.sergei.emacs.dired .profile'
