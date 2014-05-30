7c02f3abcb1e5aadc7dd74dde159c091a00cf4a8
Sergei Nosov <sergei.nosov@gmail.com>
2014-05-30 12:24:43 +0400

    add toc to readme

--- org-toc.el
+++ org-toc.el
@@ -1,3 +1,5 @@
+(defvar toc-regexp "^*.*:toc:\\($\\|[^ ]*:$\\)")
+
 (defun raw-toc ()
   (let ((content (buffer-substring-no-properties
                   (point-min) (point-max))))
@@ -5,44 +7,56 @@
       (insert content)
       (beginning-of-buffer)
       (keep-lines "^\*")
+
+      (beginning-of-buffer)
+      (re-search-forward toc-regexp)
+      (beginning-of-line)
+      (delete-region (point) (progn (forward-line 1) (point)))
+
       (buffer-substring-no-properties
        (point-min) (point-max)))))
 
 (defun hrefify-github (str)
   (let* ((spc-fix (replace-regexp-in-string " " "-" str))
          (slash-fix (replace-regexp-in-string "/" "" spc-fix)))
-    slash-fix))
+    (concat "#" slash-fix)))
 
 (defun hrefify-toc (toc hrefify)
   (with-temp-buffer
     (insert toc)
     (beginning-of-buffer)
-    (skip-chars-forward "* ")
 
-    ;; (let ((moreLines t))
-    ;;   (while (= 0 (forward-line 1))
-    ;;
-    ;;     (setq moreLines (= 0 (forward-line 1)))
-    ;;     ))
+    (while
+        (progn
+          (when (looking-at "\\*")
+            (insert "*")
+            (skip-chars-forward "* ")
+            (let* ((beg (point))
+                   (end (line-end-position))
+                   (heading (buffer-substring-no-properties
+                             beg end)))
+              (insert "[[")
+              (insert (funcall hrefify heading))
+              (insert "][")
+              (end-of-line)
+              (insert "]]"))
+            (= 0 (forward-line 1)))))
 
-    (let* ((beg (point))
-           (end (line-end-position))
-           (heading (buffer-substring-no-properties
-                     beg end)))
-      (insert "[[")
-      (insert (funcall hrefify heading))
-      (insert "][")
-      (end-of-line)
-      (insert "]]"))
     (buffer-substring-no-properties
-     (point-min) (point-max))
-    )
-  )
+     (point-min) (point-max))))
 
-(defun insert-raw-toc ()
+(defun insert-toc ()
+  (interactive)
   (save-excursion
     (beginning-of-buffer)
-    (insert (raw-toc))
-    (newline-and-indent)
-    )
-  )
+    (let ((case-fold-search t))
+      ;; find the first heading with the :TOC: tag
+      (re-search-forward toc-regexp)
+      (forward-line 1)
+
+      ;; remove previous TOC
+      (while (looking-at "\\*\\*")
+        (delete-region (point) (progn (forward-line 1) (point))))
+
+      (insert (hrefify-toc (raw-toc) 'hrefify-github))
+      )))
