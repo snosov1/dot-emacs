baa0598de8d870209403b4f014bfdeb784269689
Sergei Nosov <sergei.nosov@gmail.com>
2014-05-30 15:51:46 +0400

    ommited symbols

--- org-toc.el
+++ org-toc.el
@@ -1,3 +1,11 @@
+;; ,()%
+
+;; https://github.com/snosov1/dot-emacs#c-s,-c-r-(%D0%BD%D0%B5-%D0%B7%D0%B0%D0%B1%D1%8B%D1%82%D1%8C,-%D1%87%D1%82%D0%BE-%D0%BC%D0%BE%D0%B6%D0%BD%D0%BE-%D0%B2%D1%8B%D0%B4%D0%B5%D0%BB%D0%B8%D1%82%D1%8C-%D0%B8-%D0%BD%D0%B0%D0%B6%D0%B0%D1%82%D1%8C-%D0%B8-%D0%BE%D0%BD%D0%BE-%D0%B5%D0%B3%D0%BE-%D0%B1%D1%83%D0%B4%D0%B5%D1%82-%D0%B8%D1%81%D0%BA%D0%B0%D1%82%D1%8C),-m-s-o,-m-%-(c-q-c-j)
+;; https://github.com/snosov1/dot-emacs#m-&
+;; https://github.com/snosov1/dot-emacs#dired-(%D0%BE%D1%82%D0%BA%D1%80%D1%8B%D1%82%D1%8C-%D0%B4%D0%B8%D1%80%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%B8%D1%8E-%D0%B2-ido)
+;; https://github.com/snosov1/dot-emacs#%D0%BF%D0%B8%D1%88%D0%B5%D0%BC-%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D1%83---%D0%BA%D0%BE%D0%BC%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D1%80%D0%B8%D0%B8,-m-q
+;; https://github.com/snosov1/dot-emacs#m-%7C
+
 (defvar toc-regexp "^*.*:toc:\\($\\|[^ ]*:$\\)")
 (defvar max-depth 2)
 
