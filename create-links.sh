5adf8bbf931f2fa47f56361afd23681dfb8128e5
Sergei Nosov <sergei.nosov@gmail.com>
2014-09-18 18:05:32 +0400

    use org file

--- create-links.sh
+++ create-links.sh
@@ -1,4 +1,7 @@
-ln -s .emacs ~/.emacs
-ln -s .mc-lists.el ~/.mc-lists.el
-ln -s .abbrev_defs ~/.abbrev_defs
-ln -s .yasnippets ~/.yasnippets
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s $DIR/.emacs ~/.emacs
+ln -s $DIR/emacs-init.org ~/emacs-init.org
+ln -s $DIR/.mc-lists.el ~/.mc-lists.el
+ln -s $DIR/.abbrev_defs ~/.abbrev_defs
+ln -s $DIR/.yasnippets ~/.yasnippets
