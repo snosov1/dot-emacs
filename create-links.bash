36777b1c684dfcb92d433e6d0d04df0ff9d75c39
Sergei Nosov <sergei.nosov@gmail.com>
2014-09-19 16:46:50 +0400

    rename create-links

---
+++ create-links.bash
@@ -0,0 +1,21 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -f ~/.emacs ]; then
+    ln -s $DIR/.emacs ~/.emacs
+fi
+
+if [ ! -f ~/emacs-init.org ]; then
+    ln -s $DIR/emacs-init.org ~/emacs-init.org
+fi
+
+if [ ! -f ~/.mc-lists.el ]; then
+    ln -s $DIR/.mc-lists.el ~/.mc-lists.el
+fi
+
+if [ ! -f ~/.abbrev_defs ]; then
+    ln -s $DIR/.abbrev_defs ~/.abbrev_defs
+fi
+
+if [ ! -f ~/.yasnippets ]; then
+    ln -s $DIR/.yasnippets ~/.yasnippets
+fi
--- create-links.sh
+++
@@ -1,7 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-ln -s $DIR/.emacs ~/.emacs
-ln -s $DIR/emacs-init.org ~/emacs-init.org
-ln -s $DIR/.mc-lists.el ~/.mc-lists.el
-ln -s $DIR/.abbrev_defs ~/.abbrev_defs
-ln -s $DIR/.yasnippets ~/.yasnippets
