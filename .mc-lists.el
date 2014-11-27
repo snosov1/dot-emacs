4c5bc2ed423a5fe2adfeae7821a4bddf345b08a7
Sergei Nosov <sergei.nosov@gmail.com>
2014-11-27 11:43:21 +0300

    add C and C++ section

--- .mc-lists.el
+++ .mc-lists.el
@@ -12,6 +12,7 @@
         c-electric-semi&comma
         c-electric-slash
         c-electric-star
+        c-end-of-statement
         c-indent-line-or-region
         capitalize-dispatch
         comment-dwim
