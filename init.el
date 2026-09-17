10e3bf7adbb93c0e7adeb02b9af626f9bbdebb20
Sergei Nosov <sergei.nosov@gmail.com>
2026-09-17 10:57:35 +0300

    reverse unintended hunks

--- init.el
+++ init.el
@@ -46,15 +46,3 @@
 (require 'ob-tangle)
 
 (org-babel-load-file (concat (file-name-directory load-file-name) "emacs-init.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
